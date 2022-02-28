208145b6e31e2d01695ec78b90949b91be918bdf
RutaAuzniece <97448426+RutaAuzniece@users.noreply.github.com>
2022-02-28 15:22:14 +0200

    PedejaStunda

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ function nolasa(m1, m2, m3) {
         return { m1, m2, m3 };
     } else {
         console.log(false);
+        return false;
     }
-
 }
 
 function perimetrs(m1, m2, m3) {
@@ -32,4 +32,48 @@ const pusper= perimetrs(m1, m2, m3) / 2;
 let tlaukums=Math.sqrt(pusper*((pusper-m1)*(pusper-m2)*(pusper-m3)));
 console.log(tlaukums);
 return (tlaukums);
+}
+
+function irTrijsturis(m1,m2,m3) {
+    if (m1<m2+m3&&m2<m1+m3&&m3<m1+m2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function rezultats(){
+    const m = nolasa();
+    m1 = m.m1; m2 = m.m2; m3 = m.m3;
+    console.log(m1, m2, m3);
+    if (!nolasa()){
+        t="Nekorekti ievaddati, trijstūra malu garumiem ir jābūt lielākiem par 0";
+    }else{
+        if (irTrijsturis(m1,m2,m3)==false){
+            t="Trijstūris neeksistē, jo jebkuru divu malu summai jābūt lielākai par trešo malu!";
+        }else{
+            t="Trijstūris ar malu garumiem"+m1+","+m2+","+m3+"eksistē.";
+            if (m1==m2&&m2==m3){
+                t="Tas ir vienādmalu trijstūris.";
+            }else{
+                if(m1==m2||m2==m3||m3==m1){
+                    t+"Tas ir vienādsānu trijstūris.";
+                }
+            }
+            const p=perimetrs(m1,m2,m3);
+            const s=Math.round(laukums(m1,m2,m3)*100)/100;
+            t+="Perimetrs ir"+p+" un laukums ir"+s+".";
+        }
+    }
+    console.log(t);
+    return t;
+}
+
+function izvadaTekstu(){
+    const teksts=rezultats();
+    console.log(teksts);
+    const sakne=document.getElementById("izvade");
+    const raksti=document.createElement("p");
+    sakne.innerHTML=teksts;
+    sakne.appendChild(reksti);
 }
